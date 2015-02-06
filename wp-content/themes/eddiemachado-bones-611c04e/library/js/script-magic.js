c52290c17b2bcfd79f366862b5c721903d1081e4
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-02-06 14:20:28 +0300

    project interface-14

--- wp-content/themes/eddiemachado-bones-611c04e/library/js/script-magic.js
+++ wp-content/themes/eddiemachado-bones-611c04e/library/js/script-magic.js
@@ -369,8 +369,8 @@ jQuery('#main').on('click', '.fast-protocol', function() {
         oReader.readAsDataURL(oFile);
     }
     jQuery('#image_file').on('change', fileSelectHandler);
-    var a = new Vivus('example', {type: 'delayed', duration: 400}, jQuery(".paranja").animate({
+    var a = new Vivus('example', {type: 'delayed', duration: 400}, function(jQuery(".paranja").animate({
         opacity: 0.1,
         zIndex: -1
-      }, 1500 ));
+      }, 1500 )));
 });
