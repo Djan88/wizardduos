0d9e5acb00bc622c3ba58d63bd66544ccea1f808
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-03-11 11:00:28 +0300

    pay templates-5

--- wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
+++ wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
@@ -215,13 +215,13 @@ duos = function(){
                                 jQuery('#draggableD12').css('transform', 'rotate(-'+d12Val+'deg)');
                                 jQuery('.box_rounded').css('transform', 'rotate('+cur_animation_val+'deg) scale(1)');
                             } else if (count_animation >= 57 && count_animation <= 75){
-                                cur_animation_val += 6;
+                                cur_animation_val -= 6;
                                 d12Val+= 36;
                                 jQuery('.box_rounded').css('transform', 'rotate('+cur_animation_val+'deg) scale(1)');
                                 jQuery('#draggableD12').css('transform', 'rotate('+d12Val+'deg)');
                                 jQuery('#draggableD12').css('background', '#fff url(/wp-content/themes/eddiemachado-bones-611c04e/library/images/daemon.png) 0 0/100% no-repeat');
                             } else if (count_animation >= 75 && count_animation <= 85){
-                                cur_animation_val -= 6;
+                                cur_animation_val += 6;
                                 d12Val+= 36;
                                 jQuery('.box_rounded').css('transform', 'rotate('+cur_animation_val+'deg) scale(1)');
                                 jQuery('#draggableD12').css('transform', 'rotate('+d12Val+'deg)');
