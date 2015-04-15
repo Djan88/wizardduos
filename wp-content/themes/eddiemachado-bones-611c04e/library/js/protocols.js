48775213fbda283f2cc601e59c418251350c9cd3
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-04-15 16:49:22 +0300

    ww fix-9

--- wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
+++ wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
@@ -1535,10 +1535,10 @@ ww = function(){
                                         color: 'transparent',
                                         borderColor: 'transparent',
                                         opacity: 0.8,
-                                        transform: 'rotate(-'+d12Val/2+'deg) scale(1)',
+                                        transform: 'scale(1)',
                                         borderWidth: '1px',
                                         paddingTop: '4px',
-                                        background: '#fff url(/wp-content/themes/eddiemachado-bones-611c04e/library/images/lovushka.jpg) 0 0/100% no-repeat',
+                                        background: '#fff url(/wp-content/themes/eddiemachado-bones-611c04e/library/images/disfunction.jpg) 0 0/100% no-repeat',
                                         zIndex: '1000'
                                     });
                                     jQuery('#draggableD12')
