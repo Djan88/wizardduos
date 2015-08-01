c8f4e6b5fc740d165d919d84f8d2dd19961afe55
Ivan Avraamov <avraamov@tadatuta.com>
2015-08-01 07:38:24 +0300

    img f

--- wp-content/themes/eddiemachado-bones-611c04e/library/js/script-magic.js
+++ wp-content/themes/eddiemachado-bones-611c04e/library/js/script-magic.js
@@ -150,7 +150,7 @@ jQuery(function() {
             jQuery('.itemlist-mw, .itemlist-mm').remove();
             jQuery('.itemlist-ww').find('.example_non_anim').removeClass('hidden');
             jQuery('.itemlist-one').css({
-                background: 'url(http://aquamaritime.crimea.ua/wp-content/themes/eddiemachado-bones-611c04e/library/images/women.jpg) 50% 2px / 97% no-repeat',
+                background: 'url(/wp-content/themes/eddiemachado-bones-611c04e/library/images/women.jpg) 50% 2px / 97% no-repeat',
                 height: '1000px'
             });
         } else {
