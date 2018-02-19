670f313958d12f5c43cd0c4099d05665e42f3eff
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-02-19 15:12:33 +0300

    chart-15

--- wp-content/themes/eddiemachado-bones-611c04e/header.php
+++ wp-content/themes/eddiemachado-bones-611c04e/header.php
@@ -81,7 +81,7 @@
                             <div class="btn btn_sm btn_warning btn__wizard hidden" >Выполнить</div>
                             <div class="btn btn_sm btn_warning no_second_header no_second_btn hidden">Активирован режим с 2 фото</div>
                             <div class="btn btn_sm btn_warning btn__crop hidden" >Редактивровать фото</div>
-                            <a href="/" class="btn btn_sm btn_warning to_home">Главная</a>
+                            <a href="/" class="btn btn_sm btn_warning to_home">На главную</a>
                             <div class="btn btn_sm btn_warning btn_back invisible"><span>‹</span> Назад</div>
                         <?php }?>
 
