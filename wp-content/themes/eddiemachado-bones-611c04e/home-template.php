0d6e48623f09fbb5d9469738fa1860420b2e8d1b
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-04-19 22:10:09 +0300

    kassa-4

--- wp-content/themes/eddiemachado-bones-611c04e/home-template.php
+++ wp-content/themes/eddiemachado-bones-611c04e/home-template.php
@@ -209,7 +209,7 @@
                         <!-- <div><a href="/pay/" target="_blank" class="btn btn_lg btn_trans" style="margin-top: 10px; margin-left: 15px;">Оплата доступа</a></div> -->
                         <!-- <div><a href="/consultation/" target="_blank" class="btn btn_lg btn_trans" style="margin-top: 10px; margin-left: 15px;">Оплата консультации</a></div> -->
                     </div>
-                    <div class="login__form" id="loginform">
+                    <div class="login__form hidden" id="loginform">
                         <form name="loginform" id="loginform" action="<?php echo esc_url( site_url( 'wp-login.php', 'login_post' ) ); ?>" method="post">
                             <p>
                                 <label for="user_login"><?php _e('Username') ?><br />
@@ -230,11 +230,7 @@
                             <p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever" <?php checked( $rememberme ); ?> /> <?php esc_attr_e('Remember Me'); ?></label></p>
                             <p class="submit">
                                 <input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="<?php esc_attr_e('Log In'); ?>" />
-                                <?php   if ( $interim_login ) { ?>
-                                    <input type="hidden" name="interim-login" value="1" />
-                                <?php   } else { ?>
-                                    <input type="hidden" name="redirect_to" value="<?php echo esc_attr($redirect_to); ?>" />
-                                <?php   } ?>
+                                    <input type="hidden" name="redirect_to" value="<?php home_url(); ?>" />
                                 <?php   if ( $customize_login ) : ?>
                                     <input type="hidden" name="customize-login" value="1" />
                                 <?php   endif; ?>
