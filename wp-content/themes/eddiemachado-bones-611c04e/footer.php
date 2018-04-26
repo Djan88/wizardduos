bba717e02173d36a10cd1b5a3c45e6eb2799cb91
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-04-26 15:57:50 +0300

    home-3

--- wp-content/themes/eddiemachado-bones-611c04e/footer.php
+++ wp-content/themes/eddiemachado-bones-611c04e/footer.php
@@ -41,6 +41,7 @@
         <script src='<?php bloginfo('template_url'); ?>/library/js/easypie.js'></script>
         <script src='<?php bloginfo('template_url'); ?>/library/js/pieChartPlugin.js'></script>
         <script src='<?php bloginfo('template_url'); ?>/library/js/protocols.js'></script>
+        <script src="<?php bloginfo('template_url'); ?>/library/js/bootstrap.js"></script>
         <script src='<?php bloginfo('template_url'); ?>/library/js/script-magic.js'></script>
         <?php if(is_front_page()) { ?>
             <div class="modal fade" id="myModal_login" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
