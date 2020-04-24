21eda9f1975d025a0273a772cb51e050bceff4cd
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-04-24 14:23:50 +0300

    login-redirect

--- wp-content/themes/bcwish/functions.php
+++ wp-content/themes/bcwish/functions.php
@@ -1,12 +1,12 @@
-<?php
-function users_redirect(){
-wp_redirect(site_url('/'));
-die();
-}
+// <?php
+// function users_redirect(){
+// wp_redirect(site_url('/'));
+// die();
+// }
 
 // if(!current_user_can('manage_options')){
 // add_action('admin_init','users_redirect');
-  add_filter('login_redirect', 'users_redirect');
+  // add_filter('login_redirect', 'users_redirect');
 // }
 
 //fix for cookie error while login.
