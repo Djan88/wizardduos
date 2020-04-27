374b9ddf42333ee13c53d17bb6d10a7c3cbb2543
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-04-27 16:34:27 +0300

    front-13

--- wp-content/themes/bcwish/functions.php
+++ wp-content/themes/bcwish/functions.php
@@ -74,9 +74,9 @@ function uploadImageFile() { // Note: GD library is required for this function
 
                         //exif only supports jpg in our supported file types
                         if ($sExt == ".jpg") {
-                            print_r ($sExt);
+                            // print_r ($sExt);
                             $exif = @exif_read_data($sTempFileName);
-                            print_r ($exif);
+                            // print_r ($exif);
 
                             //get the orientation
                             if(isset($exif['Orientation'])) $orientation = $exif['Orientation'];
