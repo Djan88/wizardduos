cfa0bbb8f152d243f9f83b449a3ef86f1d822f49
Ivan Avraamov <ivan.avraamov@gmail.com>
2021-10-22 11:48:14 +0300

    kabinet

--- wp-content/themes/bcwish/index.php
+++ wp-content/themes/bcwish/index.php
@@ -103,7 +103,7 @@
         <ul>
           <?php if(is_user_logged_in()){ ?>
             <li><a href="/">Программа</a></li>
-            <li><a href="/kabinet">Личный кабинет</a></li>
+            <li><a href="'<?php echo home_url()?>. $rcl_user_URL . '">Личный кабинет</a></li>
             <?php if(current_user_can('administrator')){ ?>
               <li><a href="/wp-admin">Панель управления</a></li>
             <?php } ?>
